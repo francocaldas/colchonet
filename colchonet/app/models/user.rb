2c9fe0eeab37f3479c9e502b6b099fdf6fab6a1b
Franco Caldas <francocaldas@gmail.com>
2021-09-05 08:08:16 -0300

    aplica criptografia paa o armazenamento de senhas

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
+  validates_presence_of :email, :full_name, :location
   validates_length_of :bio, minimum: 30, allow_blank: false
   validates_uniqueness_of :email
 
   validate :email_format
+
+  has_secure_password
 
   private
     def email_format
--- colchonet/db/schema.rb
+++ colchonet/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_03_002926) do
+ActiveRecord::Schema.define(version: 2021_09_05_110404) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 2021_09_03_002926) do
   create_table "users", force: :cascade do |t|
     t.string "full_name"
     t.string "email"
-    t.string "password"
     t.string "location"
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
