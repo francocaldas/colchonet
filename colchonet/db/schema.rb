bb1834c340718dc3d902d3a59aa56d20df781212
Franco Caldas <francocaldas@gmail.com>
2021-09-22 21:16:00 -0300

    capítulo 8

--- colchonet/db/schema.rb
+++ colchonet/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_110404) do
+ActiveRecord::Schema.define(version: 2021_09_22_220240) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_09_05_110404) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.datetime "confirmed_at"
+    t.string "confirmation_token"
   end
 
 end
