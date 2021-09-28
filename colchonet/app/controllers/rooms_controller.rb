8a5df7ee972ae7d108185c6c0153819c97b2ba1e
Franco Caldas <francocaldas@gmail.com>
2021-09-27 21:50:36 -0300

    capítulo 10

--- colchonet/app/controllers/rooms_controller.rb
+++ colchonet/app/controllers/rooms_controller.rb
@@ -1,59 +1,55 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: %i[ show edit update destroy ]
+  before_action :require_authentication, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /rooms or /rooms.json
   def index
-    @rooms = Room.all
+    #@rooms = Room.all
+    @rooms = Room.most_recent.limit(4)
   end
 
   # GET /rooms/1 or /rooms/1.json
   def show
+    @room = Room.find(params[:id])
   end
 
   # GET /rooms/new
   def new
-    @room = Room.new
+    @room = current_user.rooms.build
   end
 
   # GET /rooms/1/edit
   def edit
+    @room = current_user.rooms.find(params[:id])
   end
 
   # POST /rooms or /rooms.json
   def create
-    @room = Room.new(room_params)
+    @room = current_user.rooms.build(room_params)
 
-    respond_to do |format|
-      if @room.save
-        format.html { redirect_to @room, notice: "Room was successfully created." }
-        format.json { render :show, status: :created, location: @room }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
-      end
+    if @room.save
+      redirect_to @room, notice: t('flash.notice.room_created')
+    else
+      render action: "new"
     end
   end
 
   # PATCH/PUT /rooms/1 or /rooms/1.json
   def update
-    respond_to do |format|
-      if @room.update(room_params)
-        format.html { redirect_to @room, notice: "Room was successfully updated." }
-        format.json { render :show, status: :ok, location: @room }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
-      end
+    @room = current_user.rooms.find(params[:id])
+
+    if @room.update(room_params)
+      redirect_to @room, notice: t('flash.notice.room_updated')
+    else
+      render action: "edit"
     end
   end
 
   # DELETE /rooms/1 or /rooms/1.json
   def destroy
+    @room = current_user.rooms.find(params[:id])
     @room.destroy
-    respond_to do |format|
-      format.html { redirect_to rooms_url, notice: "Room was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
+    redirect_to rooms_url
   end
 
   private
